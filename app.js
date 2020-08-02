cacab4a9ca593a2c5ac7624b3a819f282fe5cafd
your-worst-taco <stephen@tafoya.dev>
2020-08-01 22:05:06 -0600

    Restructured filesystem, switch to scss, built tileImage.js

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
+app.use('/styles', sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public/styles'),
+  indentedSyntax: false, // true = .sass and false = .scss
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ models/tileImage.js
@@ -0,0 +1,10 @@
+class TileImage {
+    constructor(size, height, width, mimetype, fileName, fullPath) {
+        this.size = size;
+        this.height = height;
+        this.width = width;
+        this.mimetype = mimetype;
+        this.fileName = fileName;
+        this.fullPath = fullPath;
+    }
+}
