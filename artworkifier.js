30d75ca2ed97b3a839a069d587c0e9ba34b67583
Stephen Tafoya <stephen@tafoya.dev>
2020-08-07 16:00:56 -0600

    Added comments

--- artworkifier.js
+++ artworkifier.js
@@ -1,7 +1,9 @@
 const ArtImage = require('./models/artImage');
 
+// import the promisify utility
 const { promisify } = require('util')
 
+// make these library functions asynchronous
 const sizeOf = promisify(require('image-size'));
 const readdir = promisify(require('fs').readdir)
 
@@ -9,12 +11,17 @@ const mime = require('mime-types');
 const path = require('path');
 const fs = require('fs');
 
+/*
+loadArtFiles
+    Asynchronous function that loads art files from given path.
+    returns array of ArtImage objects
+ */
 async function loadArtFiles (filePath) {
     let artFiles = [];
     let readFiles = await readdir(filePath, { withFileTypes: true });
 
     for (const file of readFiles) {
-        if (file.isFile())
+        if (file.isFile()) // skip any directory in artfile folder
         {
             let artImage = new ArtImage();
 
@@ -22,9 +29,11 @@ async function loadArtFiles (filePath) {
             artImage.fullPath = path.join(filePath, file.name);
             artImage.webPath = "/artwork/" + artImage.fileName;
 
+            // get a stat object from file
             let fileStats = fs.statSync(artImage.fullPath);
             artImage.size = fileStats["size"] / 1048576.0;
 
+            // get dimensions
             await sizeOf(artImage.fullPath)
                 .then( dimensions => {
                     artImage.height = dimensions.height;
@@ -32,12 +41,16 @@ async function loadArtFiles (filePath) {
                 })
                 .catch( err => console.error(err));
 
+            // set landscape variable if image is portrait
             if ( artImage.height > artImage.width )
             {
-                artImage.landscape = false;
+                artImage.landscape = false; // default is true
             }
 
+            // get mimetype
             artImage.mimetype = mime.lookup(artImage.fullPath);
+
+            // call builtin method to get thumbnail
             await artImage.loadThumbnail();
 
             artFiles.push(artImage);
@@ -47,25 +60,35 @@ async function loadArtFiles (filePath) {
     return artFiles;
 }
 
+/*
+artworkifierInit
+    Asynchronous function that is called on program start to initialize
+    the artworkifier system.
+ */
 module.exports.artworkifierInit = async function () {
     const artworkPath = path.join(__dirname, 'public/artwork');
     const artworkThumbnailPath = path.join(__dirname, 'public/artwork/thumbs');
 
+    // generate the thumbnail path and artwork path if neither presently exist
     try {
         fs.mkdirSync(artworkThumbnailPath, { recursive: true })
     } catch(err) {
         console.log("Failed to create directory..." + err);
     }
 
+    // call async loadArtFiles
     let artFiles = await loadArtFiles(artworkPath).then( files => {
         return files;
     });
 
+    // development function, remove post release
     for (const thing of artFiles) {
         console.log(thing);
     }
 
+    // export current array of artFiles for access elsewhere
     module.exports.artFiles = artFiles;
 }
 
+// export loadArtFiles for access externally
 module.exports.loadArtFiles = loadArtFiles;
--- models/artImage.js
+++ models/artImage.js
@@ -1,11 +1,28 @@
+// import the promisify utility
 const {promisify} = require('util')
 
 const path = require('path');
 const fs = require('fs');
 const sharp = require("sharp");
 
+// make these library functions asynchronous
 const readdir = promisify(require('fs').readdir)
 
+/*
+ArtImage
+    stores info for image files and contains
+    useful methods
+        - size => the file size of the image (in MBs)
+        - height => height in pixels
+        - width => width in pixels
+        - mimetype => web mimetype of file
+        - fileName => filename of image
+        - fullPath => path on local storage of image
+        - webPath => web accessible path of image
+        - thumbnail => web accessible path of image thumbnail
+        - thumbnailPath => full path on local machine of image thumbnail
+        - landscape => true if image is landscape oriented, otherwise false
+ */
 class ArtImage {
     constructor(size = 0, height = 0, width = 0, mimetype = "application/octet-stream", fileName = "", fullPath = "", webPath = "", thumbnail = "", thumbnailPath = "", landscape = true) {
         this.size = size;
@@ -20,6 +37,10 @@ class ArtImage {
         this.landscape = landscape;
     }
 
+    /*
+    loadThumbnail
+        Asynchronous function that loads or creates thumbnails for the object
+     */
     async loadThumbnail() {
         const artworkThumbnailPath = path.join(path.dirname(this.fullPath), '/thumbs');
         let thumbnailName = this.fileName + ".png";
@@ -29,6 +50,7 @@ class ArtImage {
 
         let thumbFiles = await readdir(artworkThumbnailPath);
         let found = false;
+        // attempt to find thumbnail by file name
         for (const file of thumbFiles) {
             if (file === thumbnailName) {
                 found = true;
@@ -37,14 +59,16 @@ class ArtImage {
 
         // if we make it here we need to generate a thumbnail
         if (!found) {
-            const maxFileDimension = 600;
+            const maxFileDimension = 600; // we can change this based on our design changes
             this.thumbnailPath = path.join(artworkThumbnailPath, thumbnailName);
-            if ( this.height > this.width && this.height > maxFileDimension ) { //
+            if ( this.height > this.width && this.height > maxFileDimension ) { // portrait
+                // make it fit within our specifications
                 await sharp(this.fullPath)
                     .resize({ height: 600 })
                     .toFile(this.thumbnailPath);
 
             } else if ( this.width >= this.height && this.width > maxFileDimension ) { // landscape
+                // make it fit within our specifications
                 await sharp(this.fullPath)
                     .resize({ width: 600 })
                     .toFile(this.thumbnailPath);
@@ -53,4 +77,5 @@ class ArtImage {
     }
 }
 
+// expose ArtImage to the rest of the program
 module.exports = ArtImage;
